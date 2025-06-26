324feb66eaaa604d315fb5eac8e403d251ea35c4
AntoineDehan <143590775+AntoineDehan@users.noreply.github.com>
2025-06-26 14:36:14 +0200

    Footer terminé + Légère améloration du script

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,13 @@
 const toggle = document.getElementById("burger-toggle");
-const closeBtn = document.getElementById("burger-close");
+const closeBtn = document.querySelectorAll("#burger-close");
 const links = document.getElementById("nav-anchors");
 
 toggle.addEventListener("click", () => {
   links.classList.toggle("active");
 });
 
-closeBtn.addEventListener("click", () => {
-  links.classList.toggle("active");
+closeBtn.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    links.classList.toggle("active");
+  });
 });
